519a47a0462298504e7c62d3536de674c868c024
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-17 12:07:08 +0100

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    hello
   </header>
 
   <main>
-    <TheWelcome />
+    welcome
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
