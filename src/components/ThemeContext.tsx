9c82649c368319481834e8cd99c6dedf63c77a06
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-05-08 16:50:10 +0100

    update

--- src/components/ThemeContext.tsx
+++ src/components/ThemeContext.tsx
@@ -2,15 +2,9 @@ import { ReactNode } from 'react';
 import useTheme from '../hooks/useTheme';
 import { ThemeContext } from './state-management/context/themeContext';
 
-interface ThemeContextProps {
-  children: ReactNode;
-}
-
-export const ThemeProvider = ({ children }: ThemeContextProps) => {
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   const themeState = useTheme({});
   return (
-    <ThemeContext.Provider value={themeState}>
-      {children}
-    </ThemeContext.Provider>
+    <ThemeContext.Provider value={themeState}>{children}</ThemeContext.Provider>
   );
 };
