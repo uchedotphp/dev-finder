384795a5cf9e34e3f71bb2b828cabff1f4217829
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-05-08 15:26:00 +0100

    refactor: Update Theme toggle section

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,41 +1,50 @@
 import { useContext } from 'react';
-import { ThemeContext, ThemeMode } from './state-management/context/themeContext';
+import {
+  ThemeContext,
+  ThemeMode,
+} from './state-management/context/themeContext';
 
 export default function ThemeToggle() {
   const theme = useContext(ThemeContext);
-  
+
   if (!theme) {
     return null;
   }
-  
+
   const { mode, setMode } = theme;
-  
+
+  const themes = [
+    {
+      name: 'Light',
+      value: 'light',
+    },
+    {
+      name: 'Dark',
+      value: 'dark',
+    },
+    {
+      name: 'System',
+      value: 'system',
+    },
+  ];
+
   const handleThemeChange = (newMode: ThemeMode) => {
     setMode(newMode);
   };
-  
+
   return (
-    <div className="theme-toggle">
+    <section className="theme-toggle">
       <div className="modes">
-        <button 
-          className={`mode-btn ${mode === 'light' ? 'active' : ''}`}
-          onClick={() => handleThemeChange('light')}
-        >
-          Light
-        </button>
-        <button 
-          className={`mode-btn ${mode === 'dark' ? 'active' : ''}`}
-          onClick={() => handleThemeChange('dark')}
-        >
-          Dark
-        </button>
-        <button 
-          className={`mode-btn ${mode === 'system' ? 'active' : ''}`}
-          onClick={() => handleThemeChange('system')}
-        >
-          System
-        </button>
+        {themes.map(({ name, value }) => (
+          <button
+            key={value}
+            className={`mode-btn ${mode === value ? 'active' : ''}`}
+            onClick={() => handleThemeChange(value as ThemeMode)}
+          >
+            {name}
+          </button>
+        ))}
       </div>
-    </div>
+    </section>
   );
 }
